763495680976388c0d09a95a3c554746dd5b01b3
Jim <mentha5681@gmail.com>
2021-02-20 16:48:23 +0800

    update

---
+++ MigrationDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MigrationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
